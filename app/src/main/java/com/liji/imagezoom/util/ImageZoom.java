c40e37c37d26c119ab58cf07022e8a0c5fe39270
crazyandcoder <crazyandcoder@gmail.com>
2016-07-19 11:15:15 +0800

    first commit

---
+++ app/src/main/java/com/liji/imagezoom/util/ImageZoom.java
@@ -0,0 +1,31 @@
+package com.liji.imagezoom.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.liji.imagezoom.activity.ImagePagerActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 作者：liji on 2016/7/18 11:03
+ * 邮箱：lijiwork@sina.com
+ */
+public class ImageZoom {
+
+    /**
+     * 跳转到图片预览页面
+     *
+     * @param context
+     * @param positon 图片显示的页码
+     * @param list    图片URL
+     */
+    public static void show(Context context, int positon, List<String> list) {
+        Intent intent = new Intent(context, ImagePagerActivity.class);
+        // 图片url,为了演示这里使用常量，一般从数据库中或网络中获取
+        intent.putStringArrayListExtra(ImagePagerActivity.EXTRA_IMAGE_URLS, (ArrayList<String>) list);
+        intent.putExtra(ImagePagerActivity.EXTRA_IMAGE_INDEX, positon);
+        context.startActivity(intent);
+    }
+}
---
+++ demo/src/main/java/com/liji/demo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.liji.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.liji.imagezoom.activity.ImagePagerActivity;
+import com.liji.imagezoom.util.ImageZoom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String[] urls = new String[]{"http://img0.bdstatic.com/img/image/shouye/leimu/mingxing.jpg",
+                "http://img0.bdstatic.com/img/image/shouye/leimu/mingxing.jpg",
+                "http://img0.bdstatic.com/img/image/shouye/leimu/mingxing1.jpg",
+                "http://d.hiphotos.baidu.com/album/w%3D2048/sign=14b0934b78310a55c424d9f4837d42a9/a8014c086e061d95e9fd3e807af40ad163d9cacb.jpg",
+                "http://g.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=ccd33b46d53f8794d7ff4b26e2207fc9/0d338744ebf81a4c0f993437d62a6059242da6a1.jpg",
+                "http://c.hiphotos.bdimg.com/album/s%3D900%3Bq%3D90/sign=b8658f17f3d3572c62e290dcba28121a/5fdf8db1cb134954bb97309a574e9258d0094a47.jpg",
+                "http://g.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=e58fb67bc8ea15ce45eee301863b4bce/a5c27d1ed21b0ef4fd6140a0dcc451da80cb3e47.jpg",
+                "http://f.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=6b62f61bac6eddc422e7b7f309e0c7c0/6159252dd42a2834510deef55ab5c9ea14cebfa1.jpg",
+                "http://f.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=6b62f61bac6eddc422e7b7f309e0c7c0/6159252dd42a2834510deef55ab5c9ea14cebfa1.jpg",
+                "http://g.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=e58fb67bc8ea15ce45eee301863b4bce/a5c27d1ed21b0ef4fd6140a0dcc451da80cb3e47.jpg",
+                "http://c.hiphotos.bdimg.com/album/s%3D680%3Bq%3D90/sign=cdab1512d000baa1be2c44b3772bc82f/91529822720e0cf3855c96050b46f21fbf09aaa1.jpg"
+        };
+
+        final List<String> list = new ArrayList<>();
+        for (int i = 0; i < urls.length; i++) {
+            list.add(urls[i]);
+        }
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageZoom.show(MainActivity.this, 0, list);
+            }
+        });
+
+    }
+}
